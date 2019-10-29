7898c4b2a64fb183c0abef20a4a504c9e70aad8b
Joseph Ruano <seph1603@gmail.com>
2019-10-29 14:01:58 -0500

    Form state management

--- contact/src/components/form.js
+++ contact/src/components/form.js
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styles from './form.module.css'
 
+const INITIAL_STATE = {
+	name: '',
+	email: '',
+	subject: '',
+	body: ''
+};
+
+const reducer = (state, action) => {
+	switch (action.type){
+		case 'updateFieldValue':
+			return { ...state, [action.field]:
+				action.value };
+		default:
+			return INITIAL_STATE
+	}
+}
+
 const Form = () => {
+	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+	
+	const updateFieldValue = field => event => {
+		dispatch({
+			type:  'updateFieldValue',
+			field,
+			value: event.target.value
+		});
+	}
+
 	const handleSubmit = event => {
 		event.preventDefault();
 		//TODO actually send the message
+		console.log(state);
 	}
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit}>
 			<label className={styles.label}>
 				Name
-				<input type="text" name="name" className={styles.input}/>
+				<input
+				value={state.name} 
+				type="text" 
+				name="name" 
+				className={styles.input}
+				onChange={updateFieldValue('name')}
+				/>
 			</label>
 			<label className={styles.label}>
 				Email
-				<input type="email" name="email" className={styles.input}/>
+				<input onChange={updateFieldValue('email')} value={state.email}  type="email" name="email" className={styles.input}/>
 			</label>
 			<label className={styles.label}>
 				Subject
-				<input type="text" name="subject" className={styles.input}/>
+				<input onChange={updateFieldValue('subject')} value={state.subject}  type="text" name="subject" className={styles.input}/>
 			</label>
 			<label className={styles.label}>
 				Body
-				<textarea name="body" className={styles.input}/>
+				<textarea onChange={updateFieldValue('body')}  value={state.body}  name="body" className={styles.input}/>
 			</label>
-			<button onClick={handleSubmit} className={styles.button}>Send</button>
+			<button className={styles.button}>Send</button>
 		</form>
 	);
 }
